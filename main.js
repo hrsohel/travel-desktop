07274d731a8f17c502da6e3065107eaee8e8cdde
Sohel <hrsohel679@gmail.com>
2023-09-17 19:53:38 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  Menu.setApplicationMenu(null);
+  //   win.loadFile("index.html");
+  win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
